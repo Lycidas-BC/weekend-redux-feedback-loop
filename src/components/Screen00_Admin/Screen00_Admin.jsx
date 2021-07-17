8f134dc0f98dd198004ccdd1462dd06b661a8e9f
Benji Copan <benji1220@gmail.com>
2021-07-17 18:43:52 -0500

    html basics for screen 1

--- src/components/Screen00_Admin/Screen00_Admin.jsx
+++ src/components/Screen00_Admin/Screen00_Admin.jsx
@@ -1,13 +1,16 @@
 import "./Screen00_Admin.css";
+import React from "react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 
 function Screen00_Admin() {
+
   return (
-      <>
-      </>
+    <div>
+      <h1>admin</h1>
+    </div>
   );
 }
 
--- src/components/Screen01_Feelings/Screen01_Feelings.jsx
+++ src/components/Screen01_Feelings/Screen01_Feelings.jsx
@@ -1,13 +1,56 @@
 import "./Screen01_Feelings.css";
+import React from "react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 
 function Screen01_Feelings() {
+  const [feelingInput, setFeelingInput] = useState("");
+  const [validInput, setValidInput] = useState(false);
+
+
+  //Can I borrow a feeling?
+  const handleInput = (feeling) => {
+    // make sure input is a number
+    if (!isNaN(feeling) && !isNaN(parseFloat(feeling))) {
+      if (Number(feeling) >= 0 && Number(feeling) <= 5) {
+        setFeelingInput(feeling);
+      } else {
+        setValidInput(false);
+        alert("Please make sure your number is in range.");
+      }
+    } else {
+      setValidInput(false);
+      alert("Please enter a number.");
+    }
+  }
+
+  const handleSubmit = () =>{
+
+  } //end handleSubmit
+
   return (
-      <>
-      </>
+      <div>
+        <section>
+          <h1><img src = {'images/goat_small.jpg'}></img>  Feedback <img src = {'images/goat_small.jpg'}></img></h1>
+        </section>
+        <section>
+        <form onSubmit={handleSubmit}>
+          <div><label>How are you feeling today?</label></div>
+          <div>
+            <input
+              placeholder="Enter a number up to 5"
+              value={feelingInput}
+              onChange={(evt) => handleInput(evt.target.value)}
+            />
+          </div>
+          <button type="submit" value="submit">
+            NEXT
+          </button>
+        </form>
+        </section>
+      </div>
   );
 }
 
