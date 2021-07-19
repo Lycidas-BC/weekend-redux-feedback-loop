0e2ad52c8e3b80b49c846c77a265387d79abd94c
Benji Copan <benji1220@gmail.com>
2021-07-18 19:31:08 -0500

    replaced numerical inputs with sliders, so alert is no longer necessary. still get error messages, but it works and I have a migraine so I'm calling it good

--- src/components/Screen03_Support/Screen03_Support.jsx
+++ src/components/Screen03_Support/Screen03_Support.jsx
@@ -1,4 +1,5 @@
 import "./Screen03_Support.css";
+import DiscreteSlider from "../Slider/Slider";
 import React from "react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,31 +12,12 @@ function Screen03_Support() {
   const supportDefault = useSelector((store) => store.support);
   const [supportInput, setSupportInput] = useState(supportDefault.length === 0 ? "" : supportDefault);
   
-  const validateInput = (input) => {
-    if (!isNaN(input) && !isNaN(parseFloat(input))) {
-      if (Number(input) >= 0 && Number(input) <= 5) {
-        return "valid";
-      } else {
-        return "Number out of range.";
-    }
-   } else {
-      return "Not a Number.";
-    }
-  } //end validateInput
-
   const handleSubmit = () =>{
-    // event.preventDefault();
-    const isValid = validateInput(supportInput);
-
-    if (isValid === "valid") {
-      dispatch({
-        type: "support",
-        payload: supportInput,
-      });
-      history.push('/screen4_comments');
-    } else {
-      alert(isValid);
-    }
+    dispatch({
+      type: "support",
+      payload: supportInput,
+    });
+    history.push('/screen4_comments');
   } //end handleSubmit
 
   const handlePrevious = () => {
@@ -55,11 +37,11 @@ function Screen03_Support() {
         <form onSubmit={handleSubmit}>
           <div><label>How well are you being supported today?</label></div>
           <div>
-            <input
-              placeholder="Enter a number up to 5"
-              value={supportInput}
-              onChange={(evt) => setSupportInput(evt.target.value)}
-            />
+            <DiscreteSlider 
+              sliderValue = {supportInput}
+              setSliderValue = {setSupportInput}
+              >
+            </DiscreteSlider>
           </div>
           <div className="inlineButtons">
             <button type="button" onClick={(evt) => handlePrevious()}>
